2017d571353015c39611f40f2c6552bfae2c2c12
장은경 <ekjang@datastreams.co.kr>
2021-03-11 10:27:38 +0900

    yml 설정 정보 나누기

--- front/src/info/GymUseInfoComponent.js
+++ front/src/info/GymUseInfoComponent.js
@@ -17,24 +17,17 @@ class GymUseInfoComponent extends Component {
         axios.get(server_url + "/gym-info/list")
             .then(res => {
                 this.setState({
-                    list: res.data.data,
+                    list: this.listSort(res.data.data),
                 });
             })
             .catch(res => console.log(res))
-        this.listSort()
     }
 
-    listSort = () => {
-        this.setState({list: this.state.list.sort((a, b) => {
-                //id 내림차순으로 정렬
-                return b.usingStatus - a.usingStatus;
-            })
-        })
-        this.setState({list: this.state.list.sort((a, b) => {
-                //id 오름차순으로 정렬
-                return a.id - b.id;
-            })
-        })
+    listSort = (data) => {
+        return (data.sort((a, b) => {
+            //status 내림차순으로 정렬
+            return b.usingStatus - a.usingStatus;
+        }))
     }
 
     render() {
